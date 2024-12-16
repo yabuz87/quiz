fdde980d6e7d82aaf160b968cb3a7a86b095033b
Yabets solomon <jabezsolomonz@gmail.com>
2024-12-16 23:03:19 +0300

    added some json file into package json so that i can deploy using npm run deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
+});
